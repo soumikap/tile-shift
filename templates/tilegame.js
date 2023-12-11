a1d2b1be2dec207908966aa245f1b875be7918b2
Henna <hpoon@terpmail.umd.edu>
2023-12-10 23:32:14 -0500

    just a try

--- templates/tilegame.js
+++ templates/tilegame.js
@@ -1,4 +1,314 @@
-function movetile(from, to) {
-    movingtile = document.querySelector("#to").src;
+const easy3x3 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>`
+
+const medium3x3 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>`
+
+const hard3x3 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>`
+
+const easy4x4 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>
+<div class="tile" id="x10"><img src="" alt="tile"></div>
+<div class="tile" id="x11"><img src="" alt="tile"></div>
+<div class="tile" id="x12"><img src="" alt="tile"></div>
+<div class="tile" id="x13"><img src="" alt="tile"></div>
+<div class="tile" id="x14"><img src="" alt="tile"></div>
+<div class="tile" id="x15"><img src="" alt="tile"></div>
+<div class="tile" id="x16"><img src="" alt="tile"></div>`
+
+const medium4x4 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>
+<div class="tile" id="x10"><img src="" alt="tile"></div>
+<div class="tile" id="x11"><img src="" alt="tile"></div>
+<div class="tile" id="x12"><img src="" alt="tile"></div>
+<div class="tile" id="x13"><img src="" alt="tile"></div>
+<div class="tile" id="x14"><img src="" alt="tile"></div>
+<div class="tile" id="x15"><img src="" alt="tile"></div>
+<div class="tile" id="x16"><img src="" alt="tile"></div>`
+
+const hard4x4 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>
+<div class="tile" id="x10"><img src="" alt="tile"></div>
+<div class="tile" id="x11"><img src="" alt="tile"></div>
+<div class="tile" id="x12"><img src="" alt="tile"></div>
+<div class="tile" id="x13"><img src="" alt="tile"></div>
+<div class="tile" id="x14"><img src="" alt="tile"></div>
+<div class="tile" id="x15"><img src="" alt="tile"></div>
+<div class="tile" id="x16"><img src="" alt="tile"></div>`
+
+const easy5x5 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>
+<div class="tile" id="x10"><img src="" alt="tile"></div>
+<div class="tile" id="x11"><img src="" alt="tile"></div>
+<div class="tile" id="x12"><img src="" alt="tile"></div>
+<div class="tile" id="x13"><img src="" alt="tile"></div>
+<div class="tile" id="x14"><img src="" alt="tile"></div>
+<div class="tile" id="x15"><img src="" alt="tile"></div>
+<div class="tile" id="x16"><img src="" alt="tile"></div>
+<div class="tile" id="x17"><img src="" alt="tile"></div>
+<div class="tile" id="x18"><img src="" alt="tile"></div>
+<div class="tile" id="x19"><img src="" alt="tile"></div>
+<div class="tile" id="x20"><img src="" alt="tile"></div>
+<div class="tile" id="x21"><img src="" alt="tile"></div>
+<div class="tile" id="x22"><img src="" alt="tile"></div>
+<div class="tile" id="x23"><img src="" alt="tile"></div>
+<div class="tile" id="x24"><img src="" alt="tile"></div>
+<div class="tile" id="x25"><img src="" alt="tile"></div>`
+
+const medium5x5 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>
+<div class="tile" id="x10"><img src="" alt="tile"></div>
+<div class="tile" id="x11"><img src="" alt="tile"></div>
+<div class="tile" id="x12"><img src="" alt="tile"></div>
+<div class="tile" id="x13"><img src="" alt="tile"></div>
+<div class="tile" id="x14"><img src="" alt="tile"></div>
+<div class="tile" id="x15"><img src="" alt="tile"></div>
+<div class="tile" id="x16"><img src="" alt="tile"></div>
+<div class="tile" id="x17"><img src="" alt="tile"></div>
+<div class="tile" id="x18"><img src="" alt="tile"></div>
+<div class="tile" id="x19"><img src="" alt="tile"></div>
+<div class="tile" id="x20"><img src="" alt="tile"></div>
+<div class="tile" id="x21"><img src="" alt="tile"></div>
+<div class="tile" id="x22"><img src="" alt="tile"></div>
+<div class="tile" id="x23"><img src="" alt="tile"></div>
+<div class="tile" id="x24"><img src="" alt="tile"></div>
+<div class="tile" id="x25"><img src="" alt="tile"></div>`
+
+const hard5x5 = `
+<div class="tile" id="x1"><img src="" alt="tile"></div>
+<div class="tile" id="x2"><img src="" alt="tile"></div>
+<div class="tile" id="x3"><img src="" alt="tile"></div>
+<div class="tile" id="x4"><img src="" alt="tile"></div>
+<div class="tile" id="x5"><img src="" alt="tile"></div>
+<div class="tile" id="x6"><img src="" alt="tile"></div>
+<div class="tile" id="x7"><img src="" alt="tile"></div>
+<div class="tile" id="x8"><img src="" alt="tile"></div>
+<div class="tile" id="x9"><img src="" alt="tile"></div>
+<div class="tile" id="x10"><img src="" alt="tile"></div>
+<div class="tile" id="x11"><img src="" alt="tile"></div>
+<div class="tile" id="x12"><img src="" alt="tile"></div>
+<div class="tile" id="x13"><img src="" alt="tile"></div>
+<div class="tile" id="x14"><img src="" alt="tile"></div>
+<div class="tile" id="x15"><img src="" alt="tile"></div>
+<div class="tile" id="x16"><img src="" alt="tile"></div>
+<div class="tile" id="x17"><img src="" alt="tile"></div>
+<div class="tile" id="x18"><img src="" alt="tile"></div>
+<div class="tile" id="x19"><img src="" alt="tile"></div>
+<div class="tile" id="x20"><img src="" alt="tile"></div>
+<div class="tile" id="x21"><img src="" alt="tile"></div>
+<div class="tile" id="x22"><img src="" alt="tile"></div>
+<div class="tile" id="x23"><img src="" alt="tile"></div>
+<div class="tile" id="x24"><img src="" alt="tile"></div>
+<div class="tile" id="x25"><img src="" alt="tile"></div>`
+
+
+window.onload = startgame;
+
+var gamegrid;
+
+class Grid {
+    #dim;
+    #diff;
+    #moves;
+    #board;
+    #imgheader = "/images/";
+    #emptytile;
+
+    constructor(dim, diff, griddiv) {
+        if (dim === "3x3") {
+            this.#dim = 3;
+            if (diff === "Easy") {
+                this.#board = easy3x3;
+                this.#imgheader+="easy3x3_part_0";
+            } else if (diff === "Medium") {
+                this.#board = medium3x3;
+                this.#imgheader+="medium3x3_part_0";
+            } else if (diff === "Hard") {
+                this.#board = hard3x3;
+                this.#imgheader+="hard3x3_part_0";
+            }
+        } else if (dim === "4x4") {
+            this.#dim = 4;
+            if (diff === "Easy") {
+                this.#board = easy4x4;
+                this.#imgheader+="easy4x4_part_0";
+            } else if (diff === "Medium") {
+                this.#board = medium4x4;
+                this.#imgheader+="medium4x4_part_0";
+            } else if (diff === "Hard") {
+                this.#board = hard4x4;
+                this.#imgheader+="hard4x4_part_0";
+            }
+        } else if (dim === "5x5") {
+            this.#dim = 5;
+            if (diff === "Easy") {
+                this.#board = easy5x5;
+                this.#imgheader+="easy5x5_part_0";
+            } else if (diff === "Medium") {
+                this.#board = medium5x5;
+                this.#imgheader+="medium5x5_part_0";
+            } else if (diff === "Hard") {
+                this.#board = hard5x5;
+                this.#imgheader+="hard5x5_part_0";
+            }
+        }
+        this.#moves = 0;
+        this.#diff = diff;
+        this.#emptytile = this.#dim * this.#dim;
+        griddiv.innerHTML = this.#board;
+        // add event listener for each div inside griddiv
+        for (let i = 1; i <= this.#dim * this.#dim; i++) {
+            document.getElementById("x"+i).addEventListener('click', function() {
+                const thistile = Number(this.id.substring(1));
+                let empty = getadjemptytile(thistile);
+                if (empty !== 0) {
+                    movetile(thistile, empty);
+                }
+            });
+        }
+
+    }
+
+    get dim() {
+        return this.#dim;
+    }
+
+    get imgheader() {
+        return this.#imgheader;
+    }
+
+    getadjemptytile(id) {
+        let dim = this.#dim;
+        const maxidx = this.#dim * this.#dim;
+        // 123 -> 1, 456 -> 4, 789 -> 7
+        const rowstart = ((id-1) / this.#dim) * this.#dim + 1;
+        const rowend = rowstart + (this.#dim - 1);
+
+        // if has left and left is empty, return it
+        if (id-1 >= rowstart && document.getElementById(id-1).innerHTML.trim() == "") {
+            return id-1;
+        }
+        // if has right and right is empty, return it
+        else if (id+1 <= rowend && document.getElementById(id+1).innerHTML.trim() == "") {
+            return id+1;
+        }
+        // if has up and up is empty, return it
+        else if (id - dim > 0 && document.getElementById(id-dim).innerHTML.trim() == "") {
+            return id-3;
+        }
+        // if has down and down is empty, return it
+        else if (id + dim <= maxidx && document.getElementById(id+dim).innerHTML.trim() == "") {
+            return id+3;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    movetile(from, to) {
+        const fromtile = document.getElementById("x"+from);
+        const totile = document.getElementById("x"+to);
+        const imgtomove = fromtile.innerHTML;
+        fromtile.innerHTML = "";
+        totile.innerHTML = imgtomove;
+        if (checkwin()) {
+            finishgame();
+        }
+    }
     
+    checkwin() {
+        for (let i = 1; i <= this.#dim * this.#dim; i++) {
+            let tileimg = document.getElementById("x"+i).innerHTML;
+            if (!tileimg.includes(i+".jpg")) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+function startgame() {
+    let dim = document.querySelector("#gridsize").value;
+    let diff = document.querySelector("#difficulty").value;
+    let griddiv = document.querySelector("#griddiv");
+    gamegrid = new Grid(dim, diff, griddiv);
+}
+
+function finishgame() {
+    const lasttile = document.querySelector("x" + gamegrid.dim*gamegrid.dim);
+    if (gamegrid.dim === 3) {
+        lasttile.innerHTML = `<img src="${gamegrid.imgheader}09.jpg">`
+    }else {
+        lasttile.innerHTML = `<img src="${gamegrid.imgheader}${gamegrid.dim*gamegrid.dim}.jpg">`
+    }
+    document.getElementById("finish").innerHTML = `
+    <form action="/tilegame" method="post">
+        <input type="submit" value="Finish Game"/>
+    </form>
+    `
 }
