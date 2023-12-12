497c469423164db3f256edf6d15384c70998c4ff
Henna <hpoon@terpmail.umd.edu>
2023-12-11 19:40:21 -0500

    3x3normal, 3x3hard, and all 4x4 are test friendly

--- templates/tilegame.js
+++ templates/tilegame.js
@@ -17,8 +17,8 @@ const medium3x3 = `
 <div class="t3x3" id="x5"><img src="/images/medium3x3/medium3x3_part_005.jpg" alt="tile5"></div>
 <div class="t3x3" id="x6"><img src="/images/medium3x3/medium3x3_part_006.jpg" alt="tile6"></div>
 <div class="t3x3" id="x7"><img src="/images/medium3x3/medium3x3_part_007.jpg" alt="tile7"></div>
-<div class="t3x3" id="x8"><img src="/images/medium3x3/medium3x3_part_008.jpg" alt="tile8"></div>
-<div class="t3x3" id="x9"><img src="/images/medium3x3/medium3x3_part_009.jpg" alt="tile9"></div>`
+<div class="t3x3" id="x8"></div>
+<div class="t3x3" id="x9"><img src="/images/medium3x3/medium3x3_part_008.jpg" alt="tile8"></div>`
 
 const hard3x3 = `
 <div class="t3x3" id="x1"><img src="/images/hard3x3/hard3x3_part_001.jpg" alt="tile1"></div>
@@ -28,8 +28,8 @@ const hard3x3 = `
 <div class="t3x3" id="x5"><img src="/images/hard3x3/hard3x3_part_005.jpg" alt="tile5"></div>
 <div class="t3x3" id="x6"><img src="/images/hard3x3/hard3x3_part_006.jpg" alt="tile6"></div>
 <div class="t3x3" id="x7"><img src="/images/hard3x3/hard3x3_part_007.jpg" alt="tile7"></div>
-<div class="t3x3" id="x8"><img src="/images/hard3x3/hard3x3_part_008.jpg" alt="tile8"></div>
-<div class="t3x3" id="x9"><img src="/images/hard3x3/hard3x3_part_009.jpg" alt="tile9"></div>`
+<div class="t3x3" id="x8"></div>
+<div class="t3x3" id="x9"><img src="/images/hard3x3/hard3x3_part_008.jpg" alt="tile9"></div>`
 
 const easy4x4 = `
 <div class="t4x4" id="x1"><img src="/images/easy4x4/easy4x4_part_001.jpg" alt="tile1"></div>
@@ -46,8 +46,8 @@ const easy4x4 = `
 <div class="t4x4" id="x12"><img src="/images/easy4x4/easy4x4_part_012.jpg" alt="tile12"></div>
 <div class="t4x4" id="x13"><img src="/images/easy4x4/easy4x4_part_013.jpg" alt="tile13"></div>
 <div class="t4x4" id="x14"><img src="/images/easy4x4/easy4x4_part_014.jpg" alt="tile14"></div>
-<div class="t4x4" id="x15"><img src="/images/easy4x4/easy4x4_part_015.jpg" alt="tile15"></div>
-<div class="t4x4" id="x16"><img src="/images/easy4x4/easy4x4_part_016.jpg" alt="tile16"></div>`
+<div class="t4x4" id="x15"></div>
+<div class="t4x4" id="x16"><img src="/images/easy4x4/easy4x4_part_015.jpg" alt="tile16"></div>`
 
 const medium4x4 = `
 <div class="t4x4" id="x1"><img src="/images/medium4x4/medium4x4_part_001.jpg" alt="tile1"></div>
@@ -64,8 +64,8 @@ const medium4x4 = `
 <div class="t4x4" id="x12"><img src="/images/medium4x4/medium4x4_part_012.jpg" alt="tile12"></div>
 <div class="t4x4" id="x13"><img src="/images/medium4x4/medium4x4_part_013.jpg" alt="tile13"></div>
 <div class="t4x4" id="x14"><img src="/images/medium4x4/medium4x4_part_014.jpg" alt="tile14"></div>
-<div class="t4x4" id="x15"><img src="/images/medium4x4/medium4x4_part_015.jpg" alt="tile15"></div>
-<div class="t4x4" id="x16"><img src="/images/medium4x4/medium4x4_part_016.jpg" alt="tile16"></div>`
+<div class="t4x4" id="x15"></div>
+<div class="t4x4" id="x16"><img src="/images/medium4x4/medium4x4_part_015.jpg" alt="tile16"></div>`
 
 const hard4x4 = `
 <div class="t4x4" id="x1"><img src="/images/hard4x4/hard4x4_part_001.jpg" alt="tile1"></div>
@@ -82,35 +82,35 @@ const hard4x4 = `
 <div class="t4x4" id="x12"><img src="/images/hard4x4/hard4x4_part_012.jpg" alt="tile12"></div>
 <div class="t4x4" id="x13"><img src="/images/hard4x4/hard4x4_part_013.jpg" alt="tile13"></div>
 <div class="t4x4" id="x14"><img src="/images/hard4x4/hard4x4_part_014.jpg" alt="tile14"></div>
-<div class="t4x4" id="x15"><img src="/images/hard4x4/hard4x4_part_015.jpg" alt="tile15"></div>
-<div class="t4x4" id="x16"><img src="/images/hard4x4/hard4x4_part_016.jpg" alt="tile16"></div>`
+<div class="t4x4" id="x15"></div>
+<div class="t4x4" id="x16"><img src="/images/hard4x4/hard4x4_part_015.jpg" alt="tile16"></div>`
 
 const easy5x5 = `
-<div class="t5x5" id="x1"><img src="" alt="tile"></div>
-<div class="t5x5" id="x2"><img src="" alt="tile"></div>
-<div class="t5x5" id="x3"><img src="" alt="tile"></div>
-<div class="t5x5" id="x4"><img src="" alt="tile"></div>
-<div class="t5x5" id="x5"><img src="" alt="tile"></div>
-<div class="t5x5" id="x6"><img src="" alt="tile"></div>
-<div class="t5x5" id="x7"><img src="" alt="tile"></div>
-<div class="t5x5" id="x8"><img src="" alt="tile"></div>
-<div class="t5x5" id="x9"><img src="" alt="tile"></div>
-<div class="t5x5" id="x10"><img src="" alt="tile"></div>
-<div class="t5x5" id="x11"><img src="" alt="tile"></div>
-<div class="t5x5" id="x12"><img src="" alt="tile"></div>
-<div class="t5x5" id="x13"><img src="" alt="tile"></div>
-<div class="t5x5" id="x14"><img src="" alt="tile"></div>
-<div class="t5x5" id="x15"><img src="" alt="tile"></div>
-<div class="t5x5" id="x16"><img src="" alt="tile"></div>
-<div class="t5x5" id="x17"><img src="" alt="tile"></div>
-<div class="t5x5" id="x18"><img src="" alt="tile"></div>
-<div class="t5x5" id="x19"><img src="" alt="tile"></div>
-<div class="t5x5" id="x20"><img src="" alt="tile"></div>
-<div class="t5x5" id="x21"><img src="" alt="tile"></div>
-<div class="t5x5" id="x22"><img src="" alt="tile"></div>
-<div class="t5x5" id="x23"><img src="" alt="tile"></div>
-<div class="t5x5" id="x24"><img src="" alt="tile"></div>
-<div class="t5x5" id="x25"><img src="" alt="tile"></div>`
+<div class="t5x5" id="x1"><img src="/images/easy5x5/easy5x5_part_001.jpg" alt="tile1"></div>
+<div class="t5x5" id="x2"><img src="/images/easy5x5/easy5x5_part_002.jpg" alt="tile2"></div>
+<div class="t5x5" id="x3"><img src="/images/easy5x5/easy5x5_part_003.jpg" alt="tile3"></div>
+<div class="t5x5" id="x4"><img src="/images/easy5x5/easy5x5_part_004.jpg" alt="tile4"></div>
+<div class="t5x5" id="x5"><img src="/images/easy5x5/easy5x5_part_005.jpg" alt="tile5"></div>
+<div class="t5x5" id="x6"><img src="/images/easy5x5/easy5x5_part_006.jpg" alt="tile6"></div>
+<div class="t5x5" id="x7"><img src="/images/easy5x5/easy5x5_part_007.jpg" alt="tile7"></div>
+<div class="t5x5" id="x8"><img src="/images/easy5x5/easy5x5_part_008.jpg" alt="tile8"></div>
+<div class="t5x5" id="x9"><img src="/images/easy5x5/easy5x5_part_009.jpg" alt="tile9"></div>
+<div class="t5x5" id="x10"><img src="/images/easy5x5/easy5x5_part_010.jpg" alt="tile10"></div>
+<div class="t5x5" id="x11"><img src="/images/easy5x5/easy5x5_part_011.jpg" alt="tile11"></div>
+<div class="t5x5" id="x12"><img src="/images/easy5x5/easy5x5_part_012.jpg" alt="tile12"></div>
+<div class="t5x5" id="x13"><img src="/images/easy5x5/easy5x5_part_013.jpg" alt="tile13"></div> 
+<div class="t5x5" id="x14"><img src="/images/easy5x5/easy5x5_part_014.jpg" alt="tile14"></div>
+<div class="t5x5" id="x15"><img src="/images/easy5x5/easy5x5_part_015.jpg" alt="tile15"></div>
+<div class="t5x5" id="x16"><img src="/images/easy5x5/easy5x5_part_016.jpg" alt="tile16"></div>
+<div class="t5x5" id="x17"><img src="/images/easy5x5/easy5x5_part_017.jpg" alt="tile17"></div>
+<div class="t5x5" id="x18"><img src="/images/easy5x5/easy5x5_part_018.jpg" alt="tile18"></div>
+<div class="t5x5" id="x19"><img src="/images/easy5x5/easy5x5_part_019.jpg" alt="tile19"></div>
+<div class="t5x5" id="x20"><img src="/images/easy5x5/easy5x5_part_020.jpg" alt="tile20"></div>
+<div class="t5x5" id="x21"><img src="/images/easy5x5/easy5x5_part_021.jpg" alt="tile21"></div>
+<div class="t5x5" id="x22"><img src="/images/easy5x5/easy5x5_part_022.jpg" alt="tile22"></div>
+<div class="t5x5" id="x23"><img src="/images/easy5x5/easy5x5_part_023.jpg" alt="tile23"></div>
+<div class="t5x5" id="x24"><img src="/images/easy5x5/easy5x5_part_024.jpg" alt="tile24"></div>
+<div class="t5x5" id="x25"><img src="/images/easy5x5/easy5x5_part_025.jpg" alt="tile25"></div>`
 
 const medium5x5 = `
 <div class="t5x5" id="x1"><img src="" alt="tile"></div>
@@ -173,12 +173,12 @@ window.onload = function () {
 }
 
 function startgame() {
-    // let dim = document.querySelector("#gridsize").value;
-    // let diff = document.querySelector("#difficulty").value;
+    let dim = document.querySelector("#size").innerHTML;
+    let diff = document.querySelector("#level").innerHTML;
     let griddiv = document.querySelector("#game-container");
     
-    //gamegrid = new Grid(dim, diff, griddiv);
-    gamegrid = new Grid("3x3", "easy", griddiv);
+    gamegrid = new Grid(dim, diff, griddiv);
+    //gamegrid = new Grid("3x3", "easy", griddiv);
     // add event listener for each div inside griddiv
     for (let i = 1; i <= gamegrid.dim * gamegrid.dim; i++) {
         document.getElementById("x"+i).addEventListener('click', function() {
@@ -211,34 +211,34 @@ class Grid {
             if (diff === "easy") {
                 this.#board = easy3x3;
                 this.#imgheader+="/easy3x3/easy3x3_part_0";
-            } else if (diff === "Medium") {
+            } else if (diff === "normal") {
                 this.#board = medium3x3;
                 this.#imgheader+="/medium3x3/medium3x3_part_0";
-            } else if (diff === "Hard") {
+            } else if (diff === "hard") {
                 this.#board = hard3x3;
                 this.#imgheader+="/hard3x3/hard3x3_part_0";
             }
         } else if (dim === "4x4") {
             this.#dim = 4;
-            if (diff === "Easy") {
+            if (diff === "easy") {
                 this.#board = easy4x4;
                 this.#imgheader+="/easy4x4/easy4x4_part_0";
-            } else if (diff === "Medium") {
+            } else if (diff === "normal") {
                 this.#board = medium4x4;
                 this.#imgheader+="/medium4x4/medium4x4_part_0";
-            } else if (diff === "Hard") {
+            } else if (diff === "hard") {
                 this.#board = hard4x4;
                 this.#imgheader+="/hard4x4/hard4x4_part_0";
             }
         } else if (dim === "5x5") {
             this.#dim = 5;
-            if (diff === "Easy") {
+            if (diff === "easy") {
                 this.#board = easy5x5;
                 this.#imgheader+="/easy5x5/easy5x5_part_0";
-            } else if (diff === "Medium") {
+            } else if (diff === "normal") {
                 this.#board = medium5x5;
                 this.#imgheader+="/medium5x5/medium5x5_part_0";
-            } else if (diff === "Hard") {
+            } else if (diff === "hard") {
                 this.#board = hard5x5;
                 this.#imgheader+="/hard5x5/hard5x5_part_0";
             }
@@ -257,29 +257,35 @@ class Grid {
         return this.#imgheader;
     }
 
-    getadjemptytile(id) {
+    getadjemptytile(id) { //id = 11 dim = 4
         let dim = this.#dim;
+        //alert("id: "+id+" dim: "+dim);
         const maxidx = this.#dim * this.#dim;
         // 123 -> 1, 456 -> 4, 789 -> 7
         const rowstart = Math.floor((id-1) / this.#dim) * this.#dim + 1;
         const rowend = rowstart + (this.#dim - 1);
+        //alert(rowstart+","+rowend);
         //alert("below: "+document.getElementById("x"+(id+dim)).innerHTML.trim());
 
         // if has left and left is empty, return it
         if (id-1 >= rowstart && document.getElementById("x"+(id-1)).innerHTML.trim() == "") {
+            //alert("1");
             return id-1;
         }
         // if has right and right is empty, return it
         else if (id+1 <= rowend && document.getElementById("x"+(id+1)).innerHTML.trim() == "") {
+            //alert("2");
             return id+1;
         }
         // if has up and up is empty, return it
         else if (id - dim > 0 && document.getElementById("x"+(id-dim)).innerHTML.trim() == "") {
-            return id-3;
+            //alert("3");
+            return id-dim;
         }
         // if has down and down is empty, return it
         else if (id + dim <= maxidx && document.getElementById("x"+(id+dim)).innerHTML.trim() == "") {
-            return id+3;
+            //alert("4");
+            return id+dim;
         }
         else {
             return 0;
--- tilesServer.js
+++ tilesServer.js
@@ -50,7 +50,6 @@ app.post("/", async function (request, res) {
         if (findIt) {
        		// it exists
 			// do nothing probably
-            console.log("user already exists");
    		} else { 
        		// it doesn't exist, make new json and insert
             let newuser = {username: username, lastscore: 0, highscores: {easy: [0,0,0], normal: [0,0,0], hard: [0,0,0]}}
@@ -66,19 +65,9 @@ app.post("/", async function (request, res) {
 });
 
 app.get("/tilegame", (req, res) => {
-    // so I'll need to get the requested grid size
-    // then I'll want to generate a randomized grid of that size
-    // do we want the completed picture somewhere?
-    // put that randomized grid inside of the variable and load it back up to the render
-    // should there a BIG div, for the entire grid, and I generate all the small pics as their own elements?
-    // the blank pic could have its own ID, and I can document.querySelect just that ID?
-    // document.getElementsByClassName, every piece is class tile
-    // then when a tile is clicked, I check if adjacent is a blank tile, if yes move tile
-    // have each grid have its own set "randomized grid"
-    //generateGrid();
-    //starGame();
-    // let variables = {username: currSession.username, gridsize: currSession.size, difficulty: currSession.diff};
-    res.render("tile-game");
+
+    let variables = {size: currSession.size, level: currSession.diff};
+    res.render("tile-game",variables);
 });
 
 app.post("/tilegame", async function(req, res) {
@@ -90,23 +79,59 @@ app.post("/tilegame", async function(req, res) {
     const client = new MongoClient(uri);
     try {
         await client.connect();
-        // see if score is higher than highscore
+        // first update last score
+        const filter = {username: currSession.username};
+        const updatelastscore = {$set: {lastscore: moves}};
+        const result = await client.db(databaseAndCollection.db)
+                .collection(databaseAndCollection.collection)
+                .updateOne(filter, updatelastscore);
+
+        // now figure out hs and if that should be updated
         const findIt = await client.db(databaseAndCollection.db)
                 .collection(databaseAndCollection.collection)
                 .findOne({username: currSession.username});
-        const hs = findIt.highscores.easy[0];
+        let hs;
+        let update;
+        if (currSession.diff === "easy") {
+            if (currSession.size === "3x3") {
+                hs = findIt.highscores.easy[0];
+                update = {$set: {"highscores.easy.0": moves}};
+            } else if (currSession.size === "4x4") {
+                hs = findIt.highscores.easy[1];
+                update = {$set: {"highscores.easy.1": moves}};
+            } else if (currSession.size === "5x5") {
+                hs = findIt.highscores.easy[2];
+                update = {$set: {"highscores.easy.2": moves}};
+            }
+        } else if (currSession.diff === "normal") {
+            if (currSession.size === "3x3") {
+                hs = findIt.highscores.normal[0];
+                update = {$set: {"highscores.normal.0": moves}};
+            } else if (currSession.size === "4x4") {
+                hs = findIt.highscores.normal[1];
+                update = {$set: {"highscores.normal.1": moves}};
+            } else if (currSession.size === "5x5") {
+                hs = findIt.highscores.normal[2];
+                update = {$set: {"highscores.normal.2": moves}};
+            }
+        } else if (currSession.diff === "hard") {
+            if (currSession.size === "3x3") {
+                hs = findIt.highscores.hard[0];
+                update = {$set: {"highscores.hard.0": moves}};
+            } else if (currSession.size === "4x4") {
+                hs = findIt.highscores.hard[1];
+                update = {$set: {"highscores.hard.1": moves}};
+            } else if (currSession.size === "5x5") {
+                hs = findIt.highscores.hard[2];
+                update = {$set: {"highscores.hard.2": moves}};
+            }
+        }
         currSession.hs = hs;
 
         // moves is less than highscore, update highscore
         if (hs === 0 || Number(hs) > moves) {
             const filter = {username: currSession.username};
-            const update = {$set: {"highscores.easy.0": moves, lastscore: moves}};
-            const result = await client.db(databaseAndCollection.db)
-                .collection(databaseAndCollection.collection)
-                .updateOne(filter, update);
-        } else { // else just update lastscore
-            const filter = {username: currSession.username};
-            const update = {$set: {lastscore: moves}};
+            //const update = {$set: {"highscores.easy.0": moves, lastscore: moves}};
             const result = await client.db(databaseAndCollection.db)
                 .collection(databaseAndCollection.collection)
                 .updateOne(filter, update);
